0449ad53e8b5d820c59430d5bf51c68f930a596c
Muflay <muflihattamirmuslim@gmail.com>
2026-05-18 15:08:12 +0700

    noOverriding - baseClass perkenalan

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+
+class baseClass {
+public:
+    virtual void perkenalan() final{
+        cout << "Hai Kids saya Function dari base Class";
+    }
+};
